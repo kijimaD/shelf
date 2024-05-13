8f5e3ce11c372d3e26f72fefab2c5932a1c14eea
Kijima Daigo <norimaking777@gmail.com>
2024-05-13 22:17:54 +0900

    feat: ページゲージを表示する

--- src/routers/static/pdfjs/web/extend.js
+++ src/routers/static/pdfjs/web/extend.js
@@ -3,7 +3,8 @@
 document.addEventListener("webviewerloaded", () => {
     PDFViewerApplication.initializedPromise.then(() => {
         PDFViewerApplication.eventBus.on("pagechanging", (e) => {
-            console.log('pagechanging, from ' + e.previous + ' to ' + e.pageNumber);
+            // const msg = 'pagechanging, from ' + e.previous + ' to ' + e.pageNumber;
+            updatePageGauge();
         });
     });
 });
@@ -18,3 +19,14 @@ function addBookmark() {
     var hash = window.location.hash;
     window.location.hash = `zoom=${zoom}&page=${page}`;
 };
+
+function updatePageGauge() {
+    const pageGauge = document.getElementById('pageGauge');
+    const cur = PDFViewerApplication.pdfViewer.currentPageNumber;
+    const all = PDFViewerApplication.pdfDocument.numPages;
+    const ratio = cur / all;
+    const right = '#'.repeat(20 * ratio);
+    const left = '_'.repeat(20 * (1 - ratio))
+    const content = right + '@' + left;
+    pageGauge.textContent = content;
+}
